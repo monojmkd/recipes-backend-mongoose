3914085e5a04987562c3fc4d79f3d11cbcbebc4a
monojmkd <monoj.mkd@hotmail.com>
2025-07-25 01:17:21 +0530

    updated new routes

--- functions/recipe.function.js
+++ functions/recipe.function.js
@@ -1,5 +1,6 @@
 const Recipe = require("../model/recipe.model");
 
+// add
 const addRecipe = async (newRecipe) => {
   try {
     const recipe = await Recipe.create(newRecipe);
@@ -9,6 +10,7 @@ const addRecipe = async (newRecipe) => {
   }
 };
 
+// getallRecipe
 const getAllRecipes = async () => {
   try {
     const allRecipes = await Recipe.find();
@@ -18,4 +20,80 @@ const getAllRecipes = async () => {
   }
 };
 
-module.exports = { addRecipe, getAllRecipes };
+// get Recipe By title
+const getRecipeByTitle = async (title) => {
+  try {
+    const recipe = await Recipe.find({ title: title });
+    return recipe;
+  } catch (error) {
+    throw error;
+  }
+};
+//get recipe by author
+const getRecipeByAuthor = async (author) => {
+  try {
+    const recipe = await Recipe.find({ author: author });
+    return recipe;
+  } catch (error) {
+    throw error;
+  }
+};
+//get recipe by difficulty level
+const getRecipeByDifficulty = async (difficulty) => {
+  try {
+    const recipe = await Recipe.find({ difficulty: difficulty });
+    return recipe;
+  } catch (error) {
+    throw error;
+  }
+};
+//update recipe's difficulty
+const updateRecipeDifficultyById = async (recipeId, dataToUpdate) => {
+  try {
+    const recipe = await Recipe.findByIdAndUpdate(
+      { id: recipeId },
+      dataToUpdate,
+      {
+        new: true,
+      }
+    );
+    return recipe;
+  } catch (error) {
+    throw error;
+  }
+};
+// update a recipe's prep time and cook time with the help of its title
+const updateRecipePrepAndCookTimeByTitle = async (title, dataToUpdate) => {
+  try {
+    const recipe = await Recipe.findOneAndUpdate(
+      { title: title },
+      dataToUpdate,
+      {
+        new: true,
+      }
+    );
+    return recipe;
+  } catch (error) {
+    throw error;
+  }
+};
+//delete a recipe with the help of a recipe id.
+const deleteRecipeById = async (recipeId) => {
+  try {
+    const recipe = await Recipe.findByIdAndDelete(recipeId);
+    return recipe;
+  } catch (error) {
+    throw error;
+  }
+};
+
+module.exports = {
+  addRecipe,
+  getAllRecipes,
+  getRecipeByTitle,
+  getRecipeByAuthor,
+  getRecipeByDifficulty,
+  updateRecipeDifficultyById,
+  updateRecipePrepAndCookTimeByTitle,
+  deleteRecipeById,
+};
--- route/recipe.route.js
+++ route/recipe.route.js
@@ -1,5 +1,14 @@
 const express = require("express");
-const { addRecipe, getAllRecipes } = require("../functions/recipe.function");
+const {
+  addRecipe,
+  getAllRecipes,
+  getRecipeByTitle,
+  getRecipeByAuthor,
+  getRecipeByDifficulty,
+  updateRecipeDifficultyById,
+  updateRecipePrepAndCookTimeByTitle,
+  deleteRecipeById,
+} = require("../functions/recipe.function");
 const router = express.Router();
 
 router.post("/", async (req, res) => {
@@ -20,6 +29,58 @@ router.get("/", async (req, res) => {
     res.status(200).json({ message: "All recipes are:", recipes: allRecipes });
   } catch (error) {
     res.status(500).json({ message: "Error reading all recipes", error });
+  }
+});
+
+router.get("/:title", async (req, res) => {
+  try {
+    const recipe = await getRecipeByTitle(req.params.title);
+    res.status(200).json({ message: "Recipe Details: ", recipe: recipe });
+  } catch (error) {
+    res.status(500).json("Error: ", error);
+  }
+});
+router.get("/:author", async (req, res) => {
+  try {
+    const recipe = await getRecipeByAuthor(req.params.author);
+    res.status(200).json({ message: "Recipe Details: ", recipe: recipe });
+  } catch (error) {
+    res.status(500).json("Error: ", error);
+  }
+});
+router.get("/:difficulty", async (req, res) => {
+  try {
+    const recipe = await getRecipeByDifficulty(req.params.difficulty);
+    res.status(200).json({ message: "Recipe Details: ", recipe: recipe });
+  } catch (error) {
+    res.status(500).json("Error: ", error);
+  }
+});
+router.put("/:id", async (req, res) => {
+  try {
+    const recipe = await updateRecipeDifficultyById(req.params.id, req.body);
+    res.status(200).json({ message: "Updated Recipe: ", recipe: recipe });
+  } catch (error) {
+    res.status(500).json("Error: ", error);
+  }
+});
+router.put("/:title", async (req, res) => {
+  try {
+    const recipe = await updateRecipePrepAndCookTimeByTitle(
+      req.params.title,
+      req.body
+    );
+    res.status(200).json({ message: "Updated Recipe: ", recipe: recipe });
+  } catch (error) {
+    res.status(500).json("Error: ", error);
+  }
+});
+router.delete("/:id", async (req, res) => {
+  try {
+    const recipe = await deleteRecipeById(req.params.id);
+    res.status(200).json({ message: "Deleted Recipe: ", recipe: recipe });
+  } catch (error) {
+    res.status(500).json("Error: ", error);
   }
 });
 
